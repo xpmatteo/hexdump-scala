63b195afa1ac99faeb7cc05c109d5879efcb069f
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-04-03 17:10:59 +0200

    Progress!

--- src/main/scala/HexDump.scala
+++ src/main/scala/HexDump.scala
@@ -1,6 +1,28 @@
+import java.io._
+import String._
 
 object HexDump {
+  
+  def bytes(stream: InputStream): List[Int] = {
+    var result = Nil: List[Int]
+    var c = stream.read
+    while (c != -1) {
+      result = c :: result
+      c = stream.read
+    }
+    return result.reverse
+  }
+
+  def toHex(x: Int) = f"$x%02x"
+  
+  def split[T](n: Int, values: List[T]): List[List[T]] = {
+    if (values.isEmpty)
+      Nil
+    else
+      List(values.take(n)) ::: split(n, values.drop(n))
+  }
+  
 	def	main(args: Array[String]) {
-		println("Hello, world!")
+		println(bytes(new FileInputStream(args(0))).map(toHex).mkString(" "))
 	}
 }
--- src/test/scala/HexDumpSuite.scala
+++ src/test/scala/HexDumpSuite.scala
@@ -1,32 +1,9 @@
 import org.scalatest.FunSuite
 import java.io._
+import HexDump._
 
 class HexDumpSuite extends FunSuite {
-  def bytes(stream: InputStream): List[Int] = {
-    var result = Nil: List[Int]
-    var c = stream.read
-    while (c != -1) {
-      result = c :: result
-      c = stream.read
-    }
-    return result.reverse
-  }
-
-	def toHex(x: Int) = Integer.toString(x, 16)
-	
-	def	split[T](n: Int, values: List[T]): List[List[T]] = {
-		if (values.isEmpty)
-			Nil
-		else
-			List(values.take(n)) ::: split(n, values.drop(n))
-	}
-	
-	def	formatLine(values: List[String]): String = {
-		"%s %s %s" format values
-	}
-
-	// -----
-
+  
   test("return a list of chars") {
     val stream = new ByteArrayInputStream("abc".getBytes);
     assert(List(97,98,99) == bytes(stream))
@@ -34,22 +11,19 @@ class HexDumpSuite extends FunSuite {
   
   test("format num to hex") {
     assert("20" === toHex(32))
+    assert("0a" === toHex(10))
   }
 
   test("format list of numbers to hex") {
-    assert(List("aa", "a", "11") === List(170, 10, 17).map(toHex))
+    assert(List("aa", "0a", "11") === List(170, 10, 17).map(toHex))
   }
 
-	test("format list to 3 columns") {
-   	assert(List() === split(3, List()));
+  test("format list to 3 columns") {
+    assert(List() === split(3, List()));
     assert(List(List(1)) === split(3, List(1)));
     assert(List(List(1,2,3)) === split(3, List(1,2,3)));
     assert(List(List(1,2,3), List(4)) === split(3, List(1,2,3,4)));
-   	assert(List(List(1,2,3), List(4,5,6)) === split(3, List(1,2,3,4,5,6)))
-	}
-
-	test("format list of strings to output") {
-		assert("aa bb cc" === formatLine(List("aa", "bb", "cc")))
-	}
-	
+    assert(List(List(1,2,3), List(4,5,6)) === split(3, List(1,2,3,4,5,6)))
+  }
+ 
 }
