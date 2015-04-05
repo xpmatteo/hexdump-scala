c8225fed129542c80efc5e8739a984075afb2f9c
Matteo Vaccari <mvaccari@thoughtworks.com>
2015-04-05 09:10:29 +0200

    Add a challenging acceptance test!

--- script/acceptance-test.sh
+++ script/acceptance-test.sh
@@ -22,3 +22,12 @@ hexdump $input > $expected_output
 script/hexdump $input > $actual_output
 diff -w $expected_output $actual_output
 echo "Case png file passed"
+
+echo -n "Trying an infinite file..."
+cat > $expected_output <<EOF
+0000000 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+0000010 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+EOF
+script/hexdump /dev/zero | head -2 > $actual_output
+diff -w $expected_output $actual_output
+echo "OK!"
